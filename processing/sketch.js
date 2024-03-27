a7edcdbabe83d5a3054b60142c54f14e5e144bce
CrunchOff <62104547+CrunchOff@users.noreply.github.com>
2024-03-27 16:56:10 +0100

    Update sketch.js
    responsive sketch

--- processing/sketch.js
+++ processing/sketch.js
@@ -1,14 +1,14 @@
-// Ellipse Moving
-
 let x;
 let y;
 let diameter;
 let fadeAmount = 0;
+let width;
 
 function setup() {
-  var canvas = createCanvas(1000, 80);
+  var canvas = createCanvas(windowWidth/2, 80);
   canvas.parent('sketch-holder');
   background(10);
+  width = windowWidth/2;
   x = 0;
   y = 0;
   r = width / 2;
@@ -17,19 +17,24 @@ function setup() {
 
 function draw() {
   // Effacer l'écran avec une couleur de fond transparente
-  let t = map(millis(), 0, 200000, 0, 255);
-  background(10, 10, 10, t);
+  let t = map(millis(), 0, 100000, 0, 255);
+  background(8, 11, 13, t);
 
   // Dessiner l'ellipse
-  stroke(x/2, 120, y*20);
+  stroke(x/2, 140, y*20);
   noFill();
   ellipse(x, y, diameter, diameter);
 
   // Mettre à jour les variables
-  x = cos(millis()*0.0005)*460 + 500;
+  x = cos(millis()*0.0005)*(width/2.1) + (width/2);
   y = cos(millis()*0.00045)*20 + 40;
-  diameter = y/1.8;
+  diameter = y/2;
 
   // Augmenter la valeur de fondu
   fadeAmount = constrain(fadeAmount + 1, 0, 255);
+}
+
+// Redimensionner le canevas lorsque la taille de la fenêtre change
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
