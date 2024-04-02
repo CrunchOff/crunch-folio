a2b8d0d9201d8582549c0eaffe61520050eadde3
CrunchOff <62104547+CrunchOff@users.noreply.github.com>
2024-04-02 16:30:02 +0200

    Update sketch.js

--- processing/sketch.js
+++ processing/sketch.js
@@ -5,13 +5,9 @@ let fadeAmount = 0;
 let width;
 
 function setup() {
-  if (windowWidth < 768) { // Vérifier si la largeur de la fenêtre est inférieure à 768px (taille d'écran typique des téléphones)
-    var canvas = createCanvas(windowWidth-20, 80);
-  } else {
-    var canvas = createCanvas(windowWidth/2.6, 80);
-  }
+  var canvas = createCanvas(windowWidth, 80);
   canvas.parent('sketch-holder');
-  background(10);
+  background(8, 11, 13);
   width = canvas.width; // Utiliser la largeur du canevas plutôt que la largeur de la fenêtre
   x = 0;
   y = 0;
@@ -20,22 +16,18 @@ function setup() {
 }
 
 function draw() {
-  // Effacer l'écran avec une couleur de fond transparente
-  let t = map(millis(), 0, 1000000, 0, 255);
-  background(8, 11, 13, t);
 
   // Dessiner l'ellipse
-  stroke(x/2, 140, y*20);
-  noFill();
+  //fill(y, x/8, x);
+  fill(8,11,13);
+  //noFill();
+  stroke(255);
   ellipse(x, y, diameter, diameter);
 
   // Mettre à jour les variables
-  x = cos(millis()*0.0005)*(width/2.1) + (width/2);
-  y = cos(millis()*0.00045)*20 + 40;
-  diameter = y/2;
-
-  // Augmenter la valeur de fondu
-  fadeAmount = constrain(fadeAmount + 1, 0, 255);
+  x = -sin(millis()*0.0008)*(width/2.2) + (width/2.1);
+  y = cos(millis()*0.00402)*40 + 40;
+  diameter = x/20 + 15;
 }
 
 // Redimensionner le canevas lorsque la taille de la fenêtre change
